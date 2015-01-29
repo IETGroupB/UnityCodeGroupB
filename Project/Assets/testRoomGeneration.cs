d3d0178dbd8700eb0beed1a7423a35ab7a89fb6d
Jer <jeremiah.n.dunn@gmail.com>
2015-01-29 14:56:58 +0000

    room gen now works with 2*2 -> X*Y dimensions and added more functions

--- Project/Assets/testRoomGeneration.cs
+++ Project/Assets/testRoomGeneration.cs
@@ -9,6 +9,18 @@ public class testRoomGeneration : MonoBehaviour {
 
         Debug.Log(rg.ToString());
 
+        var points = rg.getSolutionPath();
+        foreach (Point p in points)
+        {
+            Debug.Log(p);
+        }
+
+        var a = new Point(0, 0);
+        var b = new Point(2, 1);
+        var c = new Point(2, 1);
+
+        Debug.Log("Equals works? " + b.Equals(c));
+        Debug.Log("Not equals works? " + !a.Equals(b));
 	}
 
     // Update is called once per frame
