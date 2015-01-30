3314c5c82b527e1cbe8a13f535fc7a9e98221092
saloni6891 <saloni.6891@gmail.com>
2015-01-30 23:44:12 +0000

    added file handling script which will return 2D array for tile value

---
+++ Project/Assets/_scripts/LevelGeneration/FileHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public static class FileHandler{
+
+	public static int mapWidth = 16;
+
+	public static int[,] Load(string filePath){
+		try{
+			Debug.Log("Loading File...");
+			using(StreamReader sr = new StreamReader(filePath)){
+				string input = sr.ReadToEnd();
+				string[] lines = input.Split(new[]{'\r','\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+				int[,] tiles = new int[lines.Length, mapWidth];
+				Debug.Log("Parsing...");
+				for(int i = 0; i<lines.Length; i++)
+				{
+					string st = lines[i];
+					string[] nums = st.Split(new[] {',' });
+					if(nums.Length != mapWidth){
+					}
+					for(int j = 0; j< Mathf.Min(nums.Length, mapWidth); j++){
+						int val;
+						if(int.TryParse(nums[j], out val)){
+							tiles[i,j] = val;
+						}else{
+							tiles[i, j] = 1;
+						}
+					}
+				}
+				Debug.Log("Parsing Completed!");
+				return tiles;
+			}
+		}
+		catch(IOException e){
+			Debug.Log(e.Message);
+		}
+		return null;
+	}
+}
