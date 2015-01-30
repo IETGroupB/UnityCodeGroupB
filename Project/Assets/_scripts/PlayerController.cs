2df1935739921979ea4fa656edc93cd5c34caf77
coolriff <coolriff@gmail.com>
2015-01-30 15:30:30 +0000

    Player controller
    Done without player animation.

---
+++ Project/Assets/_scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float maxSpeed = 1f;
+	bool facingRight = true;
+	public float jumpForce = 700f;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if(move > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if (move < 0 && facingRight)
+		{
+			Flip();
+		}
+	}
+
+	void Update () 
+	{
+		if(grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody2D.AddForce(new Vector2(0,jumpForce));
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
