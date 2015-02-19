032c5ed1d4cd05b54a4716e90fc668791f763776
Jer <jeremiah.n.dunn@gmail.com>
2015-02-19 19:11:29 +0000

    changed start to awake

--- Project/Assets/_scripts/LevelGeneration/TilePrefabManager.cs
+++ Project/Assets/_scripts/LevelGeneration/TilePrefabManager.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class TilePrefabManager : MonoBehaviour {
     public Hashtable tileGameObjects { get; private set; }
-
-    private string tilePrefabsFolder = "Tiles/";
     
-    void Start()
+    void Awake()
     {
+        Debug.Log("start tile manager");
         tileGameObjects = new Hashtable();
         // load tile prefabs
         foreach (TileType tileType in Tile.prefabs.Keys)
         {
-            GameObject currentTile = Resources.Load(tilePrefabsFolder + Tile.prefabs[tileType]) as GameObject;
+            GameObject currentTile = Resources.Load(Tile.prefabs[tileType]) as GameObject;
             currentTile.GetComponent<Tile>().type = tileType;
             tileGameObjects.Add(tileType, currentTile);
         }
