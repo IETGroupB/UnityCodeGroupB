8559acad851f72a70ccff00eb0a7eacc8603e00a
Jer <jeremiah.n.dunn@gmail.com>
2015-02-24 16:13:30 +0000

    GP-12: updated character and added death
    -created assets for player character
    -added code to break player into separate pieces on death

--- Project/Assets/_scripts/PlayerController.cs
+++ Project/Assets/_scripts/PlayerController.cs
@@ -3,24 +3,33 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-    public bool Fire2Down = false;
+    
 
 	public float maxSpeed = 1f;
-	bool facingRight = true;
 	public float jumpForce = 700f;
+    public bool alive = true;
 
-	bool grounded = false;
-	public Transform groundCheck;
-	float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
+    public bool Fire2Down = false;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
 
-    public void KillPlayer()
+    private bool facingRight = true;
+	private bool grounded = false;
+    private float groundRadius = 0.2f;
+    private GameObject[] parts;
+
+    void Awake()
     {
-
+        parts = new GameObject[3];
+        parts[0] = transform.FindChild("Head").gameObject;
+        parts[1] = transform.FindChild("Body").gameObject;
+        parts[2] = transform.FindChild("Wheel").gameObject;
     }
 
 	void FixedUpdate () 
 	{
+        if (!alive) return;
+
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 
 		var move = Input.GetAxis ("Horizontal");
@@ -39,6 +48,8 @@ public class PlayerController : MonoBehaviour
 
 	void Update () 
 	{
+        if (!alive) return;
+
 		if(grounded && Input.GetButtonDown("Fire1"))
 		{
 			rigidbody2D.AddForce(new Vector2(0,jumpForce));
@@ -54,7 +65,24 @@ public class PlayerController : MonoBehaviour
         }
 	}
 
-	void Flip()
+    public void KillPlayer()
+    {
+        if (!facingRight) Flip();
+
+        Destroy(GetComponent<CircleCollider2D>());
+        Destroy(GetComponent<BoxCollider2D>());
+        Destroy(rigidbody2D);
+
+        for (var i = 0; i < parts.Length; i++)
+        {
+            parts[i].collider2D.enabled = true;
+            parts[i].AddComponent<Rigidbody2D>();
+        }
+        
+        alive = false;
+    }
+
+    void Flip()
 	{
 		facingRight = !facingRight;
 		var theScale = transform.localScale;
