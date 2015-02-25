777a68961c3d72447d21a39f7b89523ee04d773a
YaffyKoyo <qyaffy@gmail.com>
2015-02-25 16:26:44 +0000

    chasing AI and AI spawner
    Switch the spawner (SpawnPoint)  on and off by hand for now. Enemy
    chasing the character directly.

---
+++ Project/Assets/_scripts/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+	
+	public GameObject enemy;
+	public float spawnTime = 3f;
+	public Transform[] spawPoints;
+	public bool switchOn;
+	public int MaxEnemy;
+	public int numEnemy;
+	// Use this for initialization
+	void Start () {
+		numEnemy = 0;
+		MaxEnemy = 1;
+		switchOn = false;
+		//InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		
+		if (numEnemy < MaxEnemy && switchOn)
+		{
+			Spawn ();
+			numEnemy++;
+		}
+	}
+	void Spawn () {
+		//if (switchOn) {
+		int spawnPointIndex = Random.Range (0, spawPoints.Length);
+		Instantiate (enemy, spawPoints [spawnPointIndex].position, spawPoints [spawnPointIndex].rotation);
+		//}
+	}
+}
---
+++ Project/Assets/_scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	public float speed;
+	public bool switchOn;
+	//Transform player;
+	//NavMeshAgent nav;
+
+	void Start(){
+		speed = 100;
+		switchOn = true;
+	}
+
+	void Update(){
+		Vector3 movement = GameObject.FindGameObjectWithTag ("PlayerGibs").transform.position - transform.position;
+		if (switchOn) {
+			rigidbody2D.velocity= (movement * speed * Time.deltaTime);
+		}
+	}
+	//	void turnSwitch(){
+	//		if (Time.timeSinceLevelLoad > 3.0f)
+	//						switchOn = true;
+	//				else
+	//						switchOn = false;
+	//	}
+	
+
+	
+	// Update is called once per frame
+	/*void Update () {
+		nav.SetDestination (player.position);
+	}*/
+}
