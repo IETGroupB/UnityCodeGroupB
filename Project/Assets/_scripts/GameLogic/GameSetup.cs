841c4d4ce5cb11a356c2e74ae5f16020af170bd0
Jer <jeremiah.n.dunn@gmail.com>
2015-02-25 17:56:56 +0000

    GP-19: starting work on initialising game state

---
+++ Project/Assets/_scripts/GameLogic/GameSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetup : MonoBehaviour {
+    LevelGenerator levelGen;
+    SwitchSystem switchSystem;
+
+
+    public void Initialise(int width, int height, float goDownProb)
+    {
+        levelGen = gameObject.AddComponent<LevelGenerator>();
+        levelGen.GenerateLevel(width, height, goDownProb);
+        levelGen.DrawLevel();
+
+        switchSystem = gameObject.GetComponent<SwitchSystem>();
+        switchSystem.SetUp(levelGen.roomGrid);
+    }
+}
