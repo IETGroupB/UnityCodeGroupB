1ae5087c693ef70d88501abba313cdbae33ff194
Jer <jeremiah.n.dunn@gmail.com>
2015-02-26 16:53:50 +0000

    GP-19: removed double room draw

--- Project/Assets/testRoomGeneration.cs
+++ Project/Assets/testRoomGeneration.cs
@@ -5,28 +5,12 @@ public class testRoomGeneration : MonoBehaviour {
     SwitchSystem switchSys;
 
 	// Use this for initialization
-    IEnumerator Start()
+    void Start()
     {
 
-        /*LevelGenerator lg = gameObject.AddComponent<LevelGenerator>();
-        lg.GenerateLevel(4, 4, 0.4f);
-
-
-        //Debug.Log(lg.RoomGridToString());
-        lg.DrawLevel();
-
-
-        switchSys = gameObject.GetComponent<SwitchSystem>();
-        switchSys.SetUp(lg.roomGrid);*/
         GameSetup gs = gameObject.GetComponent<GameSetup>();
         gs.Initialise(4, 4, 0.4f);
-
-        Debug.Log("first");
-        yield return new WaitForSeconds(3);
-        Debug.Log("second");
-
-        gs.DestroyLevel();
-        gs.Initialise(2, 2, 0.4f);
+       
 	}
 
     // Update is called once per frame
