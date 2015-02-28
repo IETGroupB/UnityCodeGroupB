adc81ef096984683d486b39627256b78078120cc
coolriff <coolriff@gmail.com>
2015-02-28 19:51:26 +0000

    Intro manu animation version

--- Project/Assets/Sprites/Intro/Intro.cs
+++ Project/Assets/Sprites/Intro/Intro.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 namespace AssemblyCSharp
 {
-	public class NexNextLevel: MonoBehaviour
+	public class Intro: MonoBehaviour
 	{
-		public float restartDelay = 5f;         // Time to wait before restarting the level
-	
+		public static bool playIntro = true;
+		public float restartDelay = 4.5f;         // Time to wait before restarting the level
+		
 		Animator anim;                          // Reference to the animator component.
 		float restartTimer;                     // Timer to count up to restarting the level
-
+		
 		void Awake ()
 		{
 			// Set up the reference.
@@ -19,21 +20,14 @@ namespace AssemblyCSharp
 		void Update ()
 		{
 			// If the player has run out of health...
-			if(Door.endLevel == true)
+			if(playIntro == true)
 			{
-				// ... tell the animator the game is over.
-				anim.SetTrigger ("nextL");
-				
-				// .. increment a timer to count up to restarting.
+				anim.SetTrigger ("introFinish");
 				restartTimer += Time.deltaTime;
-				
-				// .. if it reaches the restart delay...
+
 				if(restartTimer >= restartDelay)
 				{
-					Intro.playIntro = false;
-					Door.endLevel = false;
-					testRoomGeneration.levelSize++;
-					// .. then reload the currently loaded level.
+					playIntro = false;
 					Application.LoadLevel(Application.loadedLevel);
 				}
 			}
