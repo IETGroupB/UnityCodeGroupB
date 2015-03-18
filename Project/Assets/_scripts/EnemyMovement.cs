64a0caef7970557c1391fc40d3eb885a33055c0d
Jer <jeremiah.n.dunn@gmail.com>
2015-03-18 17:01:08 +0000

    GP-66: updated player visuals

--- Project/Assets/_scripts/EnemyMovement.cs
+++ Project/Assets/_scripts/EnemyMovement.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 	public float speed;
-	public bool switchOn;
-	public float drainAmount;
+    public float chargeSpeed;
+    public float chargeTime;
+    public float dischargeTime;
+    public float attackDistance;
+    public float drainAmount;
+    public float shuntForce;
 	public int boltSegmentCount;
-	Light enemyLight;
+	
 	public float shockDuration;
 	public GameObject objSwitch;
 	public GameObject target;
+    public PlayerController targetScript;
+    public Color LightOn;
+    public Color LightCharge;
+    public Color LightOff;
 
 	SwitchSystem switchSystem;
 	private RoomGrid roomGrid;
@@ -17,37 +25,34 @@ public class EnemyMovement : MonoBehaviour {
 	private AudioSource enemySound;
 	private Sprite bolt;
 	private GameObject[] segments;
-	private bool isShocking = false;
-	private bool attack = false;
-	private float shockTime;
-
-	public float attackRate;
-	public float nextAttack;
+    private GameObject enemyLight;
+    public bool switchOn;
 
 
 
-	enum EnemyState{chasing, charging, attacking, ceaseFire, onFire};
+
+    enum EnemyState { chasing, charging, attack, discharging, ceaseFire, onFire };
 	EnemyState currentState;
 
 	float lastStateChange;
 
 	void Start(){
-		drainAmount = 0.15f;
-        transform.GetChild(0);
-        enemyLight = transform.FindChild("EnemyLight").GetComponent<Light>();
+        enemyLight = transform.FindChild("EnemyLight").gameObject;
+        enemyLight.GetComponent<Light>().color = LightOn;
+        enemyLight.GetComponent<TrailRenderer>().material.SetColor("_TintColor", LightOn);
+        enemyLight.GetComponent<SpriteRenderer>().color = LightOn;
+
 		switchOn = true;
 		objSwitch = GameObject.FindGameObjectWithTag ("LevelGenerator");
 		switchSystem = objSwitch.GetComponent<SwitchSystem> ();
 		enemySound = transform.GetComponent<AudioSource> ();
 
 		roomGrid = GameObject.Find("LevelGeneration").GetComponent<LevelGenerator>().roomGrid;
-		target = GameObject.Find ("Character");
+        target = GameObject.Find("Character").transform.FindChild("Body").gameObject;
+        targetScript = GameObject.Find("Character").GetComponent<PlayerController>();
 		enemySound.mute = true;
 
-		attackRate = 3.0f;
-		nextAttack = 0.0f;
-
-		currentState = EnemyState.onFire;
+        currentState = EnemyState.chasing;
 		lastStateChange = Time.time;
 
 		//bolt setup
@@ -79,107 +84,137 @@ public class EnemyMovement : MonoBehaviour {
         var targetRoomIndex = 0;
         var indexDiff = 0;
 
-
-		Vector3 movement = Vector3.Normalize(target.transform.position - transform.position);
 		if (switchOn) {
-            // set layer to NoCollision to ignore level geometry
-            gameObject.layer = 9;
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
-
-			enemySound.mute = false;
-
-			//mid-point pathfinding//////////////////////////////////////////////
-			targetRoom = roomGrid.GetClosestRoom (target.transform.position);
-			enemyRoom = roomGrid.GetClosestRoom (transform.position);
-
-            for (var i = 0; i < roomGrid.solutionPath.Length; i++)
-            {
-				if(targetRoom == roomGrid.solutionPath[i]){
-					targetRoomIndex = i;
-				}
-				if(enemyRoom == roomGrid.solutionPath[i]){
-					enemyRoomIndex = i;
-				}
-			}
-
-			indexDiff = targetRoomIndex - enemyRoomIndex;
-
-			//calculate harmming rate by distance//////////////////////////////
-			if(attack){
-				if(indexDiff<= 1 && indexDiff>= -1){
-					movement = target.transform.position-transform.position;
-					GetComponent<Rigidbody2D>().AddForce(movement * speed* Time.deltaTime);
-				}
-
-				else if(indexDiff>0){
-					movement = new Vector3(roomGrid.solutionPath[enemyRoomIndex+1].x*16+8,-(roomGrid.solutionPath[enemyRoomIndex+1].y*16+8),0)-transform.position;
-					GetComponent<Rigidbody2D>().velocity = (Vector3.Normalize(movement) * speed*indexDiff*indexDiff * Time.deltaTime);
-				}
-				else{
-					movement = new Vector3(roomGrid.solutionPath[enemyRoomIndex-1].x*16+8,-(roomGrid.solutionPath[enemyRoomIndex-1].y*16+8),0)-transform.position;
-					GetComponent<Rigidbody2D>().velocity = (Vector3.Normalize(movement) * speed*indexDiff*indexDiff * Time.deltaTime);
-				}
-			}
-
-			enemyLight.enabled = true;
-
+            
 
             switch (currentState)
             {
-                case EnemyState.ceaseFire:
-                    attack = false;
-                    movement = target.transform.position - transform.position;
-                    GetComponent<Rigidbody2D>().velocity = (Vector3.Normalize(movement) * (-50.0f) * Time.deltaTime);
-                    if (GetStateElapsed() > 1.0f)
-                        SetCurrentState(EnemyState.onFire);
+                case EnemyState.chasing:
+                    var colourOn = Color.Lerp(enemyLight.GetComponent<Light>().color, LightOn, Time.deltaTime);
+                    enemyLight.GetComponent<Light>().color = colourOn;
+                    enemyLight.GetComponent<TrailRenderer>().material.SetColor("_Color", colourOn);
+                    enemyLight.GetComponent<SpriteRenderer>().color = colourOn;
+
+                    // set layer to NoCollision to ignore level geometry
+                    gameObject.layer = 9;
+                    gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+
+                    enemySound.mute = false;
+
+                    //mid-point pathfinding//////////////////////////////////////////////
+                    targetRoom = roomGrid.GetClosestRoom(target.transform.position);
+                    enemyRoom = roomGrid.GetClosestRoom(transform.position);
+
+                    for (var i = 0; i < roomGrid.solutionPath.Length; i++)
+                    {
+                        if (targetRoom == roomGrid.solutionPath[i])
+                        {
+                            targetRoomIndex = i;
+                        }
+                        if (enemyRoom == roomGrid.solutionPath[i])
+                        {
+                            enemyRoomIndex = i;
+                        }
+                    }
+
+                    indexDiff = targetRoomIndex - enemyRoomIndex;
+
+                    if (indexDiff <= 1 && indexDiff >= -1)
+                    {
+                        var movement = Vector3.Normalize(target.transform.position - transform.position);
+                        // use forces for smoother movement when near player
+                        GetComponent<Rigidbody2D>().AddForce(movement * speed * Time.deltaTime);
+                    }
+                    else if (indexDiff > 0)
+                    {
+                        var movement = new Vector3(roomGrid.solutionPath[enemyRoomIndex + 1].x * 16 + 8, -(roomGrid.solutionPath[enemyRoomIndex + 1].y * 16 + 8), 0) - transform.position;
+                        GetComponent<Rigidbody2D>().velocity = (Vector3.Normalize(movement) * speed * indexDiff * indexDiff * Time.deltaTime);
+                    }
+                    else
+                    {
+                        var movement = new Vector3(roomGrid.solutionPath[enemyRoomIndex - 1].x * 16 + 8, -(roomGrid.solutionPath[enemyRoomIndex - 1].y * 16 + 8), 0) - transform.position;
+                        GetComponent<Rigidbody2D>().velocity = (Vector3.Normalize(movement) * speed * indexDiff * indexDiff * Time.deltaTime);
+                    }
+
+                    // can't use 2d colliders with physics layers, divide attack distance by two to prevent flicking between states
+                    if (
+                        Mathf.Pow(
+                        Mathf.Pow(transform.position.x - target.transform.position.x, 2) +
+                        Mathf.Pow(transform.position.y - target.transform.position.y, 2)
+                        , 0.5f)
+                        < attackDistance * 0.5f)
+                    {
+                        SetCurrentState(EnemyState.charging);
+                    }
                     break;
-                case EnemyState.onFire:
-                    attack = true;
-                    speed = 100;
-                    if (GetStateElapsed() > 1.5f)
-                        SetCurrentState(EnemyState.ceaseFire);
+                case EnemyState.charging:
+                    var colourCharge = Color.Lerp(enemyLight.GetComponent<Light>().color, LightCharge, Time.deltaTime / chargeTime);
+                    enemyLight.GetComponent<Light>().color = colourCharge;
+                    enemyLight.GetComponent<TrailRenderer>().material.SetColor("_Color", colourCharge);
+                    enemyLight.GetComponent<SpriteRenderer>().color = colourCharge;
+
+                    var chargeMovement = Vector3.Normalize(target.transform.position - transform.position);
+                    var distance = Mathf.Pow(
+                        Mathf.Pow(transform.position.x - target.transform.position.x, 2) +
+                        Mathf.Pow(transform.position.y - target.transform.position.y, 2)
+                        , 0.5f);
+                    // use forces for smoother movement when near player
+
+                    // don't get on top of the player
+                    if(distance < attackDistance * 0.4f)
+                        GetComponent<Rigidbody2D>().AddForce(chargeMovement * chargeSpeed * Time.deltaTime);
+
+                    //play charge sound effect
+
+
+                    // player has escaped attack
+                    if (distance > attackDistance)
+                        SetCurrentState(EnemyState.chasing);
+
+                    if (GetStateElapsed() > chargeTime)
+                        SetCurrentState(EnemyState.attack);
+
+                    break;
+                case EnemyState.attack:
+                    ShowBolt();
+                    DrawBolt(new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y));
+                    targetScript.DrainEnergy(drainAmount);
+
+                    var attackShunt = -Vector3.Normalize(target.transform.position - transform.position);
+                    GetComponent<Rigidbody2D>().AddForce(attackShunt * shuntForce);
+
+                    SetCurrentState(EnemyState.discharging);
+
+                    break;
+                case EnemyState.discharging:
+                    var colourDischarge = Color.Lerp(enemyLight.GetComponent<Light>().color, LightOn, Time.deltaTime);
+                    enemyLight.GetComponent<Light>().color = colourDischarge;
+                    enemyLight.GetComponent<TrailRenderer>().material.SetColor("_Color", colourDischarge);
+                    enemyLight.GetComponent<SpriteRenderer>().color = colourDischarge;
+
+
+                    if (GetStateElapsed() < dischargeTime * 0.5f)
+                        DrawBolt(new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y));
+                    else
+                        HideBolt();
+
+                    if (GetStateElapsed() > dischargeTime)
+                        SetCurrentState(EnemyState.chasing);
+
                     break;
             }
-
-
-
 		} else {
+            var colourOff = Color.Lerp(enemyLight.GetComponent<Light>().color, LightOff, Time.deltaTime);
+            enemyLight.GetComponent<Light>().color = colourOff;
+            enemyLight.GetComponent<TrailRenderer>().material.SetColor("_Color", colourOff);
+            enemyLight.GetComponent<SpriteRenderer>().color = colourOff;
+
             // set layer to default for collision
             gameObject.layer = 0;
             gameObject.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
 
-			enemyLight.enabled = false;
+			enemyLight.GetComponent<Light>().color = LightOff;
 			enemySound.mute = true;
-		}
-
-		if (isShocking) {
-			DrawBolt (new Vector2 (target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y));
-		
-			shockTime += Time.deltaTime;
-			if (shockTime > shockDuration)
-			{
-				shockTime = 0.0f;
-				isShocking = false;
-				HideBolt();
-			}
-		}
-	}
-
-	void OnTriggerStay2D(Collider2D other){
-		if (other.gameObject.name == "Character" && switchOn && attack) {
-            var distance = Vector3.Distance(transform.position, other.transform.position);
-			other.GetComponent<PlayerController> ().DrainEnergy (drainAmount / (distance * distance));
-		}
-	
-		if (switchOn && !isShocking && attack)
-		{
-			//GameObject other = coll.transform.gameObject;
-			if (other.name == "Character")
-			{
-				//other.GetComponent<PlayerController>().KillPlayer();	
-				isShocking = true;
-				ShowBolt();
-			}
 		}
 	}
 	
@@ -222,3 +257,4 @@ public class EnemyMovement : MonoBehaviour {
 		segments[index].transform.eulerAngles = new Vector3(0.0f, 0.0f, angle);
 	}
 }
+
