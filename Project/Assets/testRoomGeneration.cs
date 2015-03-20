b50a1ea40af50d59c3c87097a32f95f7bf1f5d51
coolriff <coolriff@gmail.com>
2015-03-20 14:28:40 +0000

    Intro

--- Project/Assets/testRoomGeneration.cs
+++ Project/Assets/testRoomGeneration.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class testRoomGeneration : MonoBehaviour {
     SwitchSystem switchSys;
 
-	public static int levelSize = 4;
+	public static int levelSize = 2;
     public float  goDownProb;
 	// Use this for initialization
     void Start()
